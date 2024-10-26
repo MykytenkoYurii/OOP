a025e68a2302f12eeb2945b376077bb6c3c5cf39
MykytenkoYurii <yura.mikitenko2005@gmail.com>
2024-10-26 17:01:12 +0300

    Refactor: Only One Level of Indentation per Method

---
+++ lab3/src/main/java/com/gildedrose/GildedRose.java
@@ -0,0 +1,73 @@
+package com.gildedrose;
+
+public class GildedRose {
+    public Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (Item item : items) {
+            updateItem(item);
+        }
+    }
+
+    private void updateItem(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            updateAgedBrie(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            updateBackstagePass(item);
+        } else {
+            updateNormalItem(item);
+        }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+
+        if (item.sellIn < 0) {
+            handleExpired(item);
+        }
+    }
+
+    private void updateAgedBrie(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void updateBackstagePass(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+
+        if (item.sellIn < 11 && item.quality < 50) {
+            item.quality++;
+        }
+
+        if (item.sellIn < 6 && item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void updateNormalItem(Item item) {
+        if (item.quality > 0) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.quality--;
+            }
+        }
+    }
+
+    private void handleExpired(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            updateAgedBrie(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
+        } else if (item.quality > 0) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.quality--;
+            }
+        }
+    }
+}
